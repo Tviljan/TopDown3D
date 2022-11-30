714d9a607a0f10528bf17ed8e903e7984bf48441
Tviljan <virallinen@gmail.com>
2022-11-30 16:08:33 +0200

    falling

--- src/Player.gd
+++ src/Player.gd
@@ -9,6 +9,7 @@ var killed = false
 var speed = 25
 func _ready():
 	camera = get_parent().find_child("Camera")
+	
 func Kill():
 	print("killed")
 	killed = true
@@ -21,7 +22,11 @@ func DeadState(delta):
 	if (!running):
 		running = true
 		robotAnimation.play("Hurt")
-	
+		
+func FallingState(delta):
+	velocity.y -= 150 * delta
+	robotAnimation.play("Fall")
+		
 func ActionState(delta):	
 	if (Input.is_anything_pressed()):
 		robotAnimation.play("Run")
@@ -39,7 +44,6 @@ func ActionState(delta):
 		var forward_direction= global_transform.basis.z.normalized()
 		global_translate(forward_direction * speed * delta)
 
-	move_and_slide()
 
 	#shoot
 	if Input.is_action_pressed("primary_action"):
@@ -53,13 +57,13 @@ func _physics_process(delta):
 	velocity = Vector3.ZERO
 		# Add the gravity.
 	if not is_on_floor():
-		velocity.y -= 100 * delta
-		
-	if killed:
+		FallingState(delta)
+	elif killed:
 		DeadState(delta)
 	else:
 		ActionState(delta)
-			
+	
+	move_and_slide()
 
 func _on_gun_controller_tree_entered():
 	pass # Replace with function body.
